b97f6a3083bc27ee6bc7177c71a8a598b9758d49
dUPYeYE <robert.boros12@gmail.com>
2023-07-07 17:07:25 +0300

    Added new polybar configfor laptop

---
+++ polybar/Laptop-versions/Catppuccin-new/launch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+# polybar main &
+
+echo "Bar launched..."
+
+MONITOR=eDP
+
+polybar main &
+
+sleep 1
+polybar secondary &
+
